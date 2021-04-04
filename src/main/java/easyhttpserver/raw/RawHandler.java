207e4782f09bd2d5c9f50accec569bc318604eb0
Maximilian W <ruhrpottattacke@gmail.com>
2021-04-04 10:58:13 +0200

    added RawHandler class

---
+++ src/main/java/easyhttpserver/raw/RawHandler.java
@@ -0,0 +1,29 @@
+package easyhttpserver.raw;
+
+import com.sun.net.httpserver.HttpExchange;
+import easyhttpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class RawHandler implements com.sun.net.httpserver.HttpHandler {
+    private final HttpHandler handler;
+
+    public RawHandler(HttpHandler handler) {
+        this.handler = handler;
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if(!exchange.getRequestMethod().equals("GET")) {
+            exchange.sendResponseHeaders(405,-1);
+            return;
+        }
+        easyhttpserver.HttpExchange easyExchange = new easyhttpserver.HttpExchange(exchange);
+        handler.handle(easyExchange);
+        if(!easyExchange.getResponse().isEmpty()) {
+            exchange.sendResponseHeaders(200, easyExchange.getResponse().length());
+            exchange.getResponseBody().write(easyExchange.getResponse().getBytes());
+            exchange.close();
+        }
+    }
+}
